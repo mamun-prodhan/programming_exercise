760011373eaf7988d5eda0c97085f5fad26df0ed
mamun-prodhan <59230847+mamun-prodhan@users.noreply.github.com>
2020-01-09 01:56:08 +0600

    Add files via upload

---
+++ basic/input_name_initial.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char name;
+    char name2;
+
+    scanf("%c",&name);
+    scanf("%c",&name2);
+
+    printf("%c\n",name);
+    printf("%c",name2);
+
+
+    return 0;
+}
