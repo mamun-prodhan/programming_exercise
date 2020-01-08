30ea8fc4447a9c5900676a51cad940f33684c6b9
mamun-prodhan <59230847+mamun-prodhan@users.noreply.github.com>
2020-01-09 01:53:33 +0600

    Add files via upload

---
+++ basic/input_your_cgpa.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    float c_gpa, c_gpa2;
+
+    scanf("%f",&c_gpa);
+    scanf("%f",&c_gpa2);
+
+    printf("%.2f\n",c_gpa);
+    printf("%.2f",c_gpa2);
+
+
+    return 0;
+}
