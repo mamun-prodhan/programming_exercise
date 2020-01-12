a788be71facd096496bfaff83ab0e3d21acc590a
mamun-prodhan <59230847+mamun-prodhan@users.noreply.github.com>
2020-01-12 12:45:54 +0600

    Add files via upload

---
+++ basic/io_full_name.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char full_name[50];
+    char full_name2[50];
+
+    gets(full_name);
+    puts(full_name);
+
+    gets(full_name2);
+    puts(full_name2);
+
+
+
+    return 0;
+}
